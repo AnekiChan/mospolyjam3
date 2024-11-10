86ed3b4649f7d979aa3e0502e6ab3fbc9278fee3
Полина <lovepolina2004@gmail.com>
2024-11-10 19:51:36 +0300

    upd

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float dodgeDistance = 3f;
     [SerializeField] private float dodgeCooldown = 1f;
+    [SerializeField] private Animator _animator;
 
     [Space]
     [Header("Glitch")]
@@ -15,6 +16,7 @@ public class PlayerController : MonoBehaviour
 
     private Vector2 movement;
     private Vector2 lastMovementDirection;
+    private Vector2 lastDirection;
     private float lastDodgeTime;
     private bool _isMoving = true;
 
@@ -40,6 +42,56 @@ public class PlayerController : MonoBehaviour
     {
         Vector3 newPosition = transform.position + (Vector3)movement * moveSpeed * Time.deltaTime;
         transform.position = newPosition;
+
+        ChangeAnimation();
+    }
+
+    private void ChangeAnimation()
+    {
+        // Если игрок не двигается, переключаемся на Idle, сбросив все триггеры
+        if (movement.magnitude == 0)
+        {
+            _animator.ResetTrigger("Up");
+            _animator.ResetTrigger("Down");
+            _animator.ResetTrigger("Right");
+            _animator.SetTrigger("Idle");
+            return; // Завершаем выполнение метода
+        }
+
+        // Если направление движения изменилось, устанавливаем триггеры для соответствующей анимации
+        if (movement != lastDirection)
+        {
+            lastDirection = movement;
+
+            if (movement.y > 0) // Движение вверх
+            {
+                _animator.ResetTrigger("Down");
+                _animator.ResetTrigger("Right");
+                _animator.SetTrigger("Up");
+                transform.localScale = new Vector3(1, 1, 1); // Сбрасываем зеркалирование
+            }
+            if (movement.y < 0) // Движение вниз
+            {
+                _animator.ResetTrigger("Up");
+                _animator.ResetTrigger("Right");
+                _animator.SetTrigger("Down");
+                transform.localScale = new Vector3(1, 1, 1);
+            }
+            if (movement.x > 0) // Движение вправо
+            {
+                _animator.ResetTrigger("Up");
+                _animator.ResetTrigger("Down");
+                _animator.SetTrigger("Right");
+                transform.localScale = new Vector3(1, 1, 1); // Сбрасываем зеркалирование
+            }
+            if (movement.x < 0) // Движение влево
+            {
+                _animator.ResetTrigger("Up");
+                _animator.ResetTrigger("Down");
+                _animator.SetTrigger("Right");
+                transform.localScale = new Vector3(-1, 1, 1); // Отражаем спрайт по оси X
+            }
+        }
     }
 
     private void Dodge()
